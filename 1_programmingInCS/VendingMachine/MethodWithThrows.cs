6b4b0e8bb44aa9b9c3e8cfb836124013c2fb200a
MikeB <mikebendorf11@gmail.com>
2017-10-12 09:49:10 -0700

    Vending Machine Project Started (My Version) UW assignments are designed to build on top of each other. For this reason, this will be the one project where all changes will be implemented from now on.
    For this commit:
    - Included the classes from ex01 that I've already worked on
    
    Next Steps:
    - Implement the changes suggested in class. A different approach will
    be used for input validation:
    	* Instead of exception handling use tryParse and loops
    	* The else statement will contain Debug.Writeline() messages
    
    After this is done we are ready for assignment N2

---
+++ 1_programmingInCS/VendingMachine/MethodWithThrows.cs
@@ -0,0 +1,39 @@
+﻿//------------------------------------------------- 
+// MethodWithThrows.cs (c) 2006 by Charles Petzold 
+//------------------------------------------------- 
+using System;
+
+static class MethodWithThrows
+{
+
+    public static uint PetzoldParse(string str)
+    {
+        uint result = 0;
+        int i = 0;
+
+        // If argument is null, throw an exception. 
+        if (str == null)
+            throw new ArgumentNullException();
+
+        // Get rid of white space. 
+        str = str.Trim();
+
+        // Check if there's at least one character. 
+        if (str.Length == 0)
+            throw new FormatException();
+
+        // Loop through all the characters in the string. 
+        while (i < str.Length)
+        {
+            // If the next character's not a digit, throw exception. 
+            if (!Char.IsDigit(str, i))
+                throw new FormatException();
+
+            // Accumulate the next digit (notice "checked"). 
+            result = checked(10 * result + (uint)str[i] - (uint)'0');
+
+            i++;
+        }
+        return result;
+    }
+}
