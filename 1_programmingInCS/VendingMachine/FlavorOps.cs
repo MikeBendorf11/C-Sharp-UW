428e77500e0986ec74482cc4d56ad5fbeb5f0e25
MikeB <mikebendorf11@gmail.com>
2017-11-03 21:03:42 -0700

    Preparation for Assignment 05
    There are a few topics to cover in the next couple days: Lists,
    Dictionaries, LINQ and Exceptions. They will all be put into practice in
    this exercise.
    
    For now I added the new FlavorOps class which will do common list operations
    with the flavor enumeration.

---
+++ 1_programmingInCS/VendingMachine/FlavorOps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MyVendingMachine
+{
+    public enum Flavor { REGULAR, ORANGE, LEMON }
+
+    public static class FlavorOps
+    {
+        private static List<Flavor> _allFlavors = new List<Flavor>();
+        static FlavorOps()
+        {
+            
+        }
+        // method to convert a string value into an enumeral
+        public static Flavor ToFlavor(string FlavorName)
+        {
+            foreach (Flavor flv in _allFlavors)
+                if (flv.ToString() == FlavorName)
+                    return flv;
+            return Flavor.REGULAR;
+        }
+        // property to return a List<Flavor> of all of the Varieties
+        public static List<Flavor> AllFlavors
+        {
+            get
+            {
+                return _allFlavors;
+            }
+        }
+    }
+}
