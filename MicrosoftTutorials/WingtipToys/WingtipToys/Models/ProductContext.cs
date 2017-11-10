754d1880ba91c6774fae0d9a57d7d26f03cb3b8c
MikeB <mikebendorf11@gmail.com>
2017-11-10 07:26:12 -0800

    moving to new branch- no exceptions

---
+++ MicrosoftTutorials/WingtipToys/WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+namespace WingtipToys.Models
+{
+    //Access the table
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
