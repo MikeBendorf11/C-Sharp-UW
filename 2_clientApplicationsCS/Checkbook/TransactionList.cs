dc7b4b4183d8a84acab4c116ff531e6d2ea382ca
MikeB <mikebendorf11@gmail.com>
2018-01-30 04:49:19 -0800

    XML Parsed Transactions print on a WPF listbox

--- 2_clientApplicationsCS/Checkbook/TransactionList.cs
+++ 2_clientApplicationsCS/Checkbook/TransactionList.cs
@@ -11,7 +11,15 @@ namespace Checkbook
 {
     class TransactionList : List<Transaction>
     {
-        enum Property { ID = 1, DATE, TYPE, DESCRIPTION, CATEGORY, AMOUNT, CHECKNUM = 8 };
+        const int ID = 1;
+        const int DATE = 2;
+        const int TYPE = 3;
+        const int DESCRIPTION = 4;
+        const int CATEGORY = 5;
+        const int AMOUNT = 6;
+        const int CHECKNUM = 8;
+        const int TOTALTRS = 10;
+
         public TransactionList()
         {
             string input = @"<Transactions><Transaction><Id>1</Id><Date>11/23/2014</Date><Type>Deposit</Type><Description>Pay</Description><Category>Income</Category><Amount>1327</Amount></Transaction><Transaction><Id>2</Id><Date>11/24/2014</Date><Type>Check</Type><Description>Food</Description><Category>Food</Category><Amount>62</Amount><Checknum>2021</Checknum></Transaction><Transaction><Id>3</Id><Date>11/24/2014</Date><Type>Debit</Type><Description>ATM</Description><Category>Misc</Category><Amount>40</Amount></Transaction><Transaction><Id>4</Id><Date>11/25/2014</Date><Type>Check</Type><Description>Rent</Description><Category>Rent</Category><Amount>713</Amount><Checknum>2022</Checknum></Transaction><Transaction><Id>5</Id><Date>11/26/2014</Date><Type>Debit</Type><Description>Dinner</Description><Category>Friends</Category><Amount>37.29</Amount></Transaction><Transaction><Id>6</Id><Date>11/26/2014</Date><Type>Debit</Type><Description>Movie</Description><Category>Friends</Category><Amount>12.50</Amount></Transaction><Transaction><Id>7</Id><Date>11/27/2014</Date><Type>Deposit</Type><Description>Mom</Description><Category>Income</Category><Amount>100</Amount></Transaction><Transaction><Id>8</Id><Date>11/28/2014</Date><Type>Check</Type><Description>Costco</Description><Category>Misc</Category><Amount>15.72</Amount><Checknum>2024</Checknum></Transaction><Transaction><Id>9</Id><Date>11/29/2014</Date><Type>Debit</Type><Description>Gas</Description><Category>Gas</Category><Amount>43.83</Amount></Transaction><Transaction><Id>10</Id><Date>11/30/2014</Date><Type>Debit</Type><Description>Market</Description><Category>Food</Category><Amount>35.11</Amount></Transaction></Transactions>";
@@ -25,54 +33,50 @@ namespace Checkbook
                                        @"(<Checknum>(\d*)<\/Checknum>)?"));
 
             MatchCollection matchCollection = pattern.Matches(input);
-            Transaction[] theTransac = new Transaction[10];
-            DateTime date = new DateTime(1980, 1, 1);
+            Transaction[] theTransac = new Transaction[TOTALTRS];
+            DateTime baseDate = new DateTime(1980, 1, 1);
             CultureInfo culture = new CultureInfo("en-US");
 
-            int countMatch = 0;
+            int countTrs = 0;
             foreach (Match m in matchCollection)
             {
-                //whole transaction parser
+                //find transaction type
                 GroupCollection gc = m.Groups;
 
-                if (gc[(int)Property.TYPE].ToString().ToUpper() == "CHECK")
-                    theTransac[countMatch] = new Check(date, "", "", 0m, "");
-                else if (gc[(int)Property.TYPE].ToString().ToUpper() == "DEBIT")
-                    theTransac[countMatch] = new Debit(date, "", "", 0m);
-                else if (gc[(int)Property.TYPE].ToString().ToUpper() == "DEPOSIT")
-                    theTransac[countMatch] = new Deposit(date, "", "", 0m);
+                if (gc[TYPE].ToString().ToUpper() == "CHECK")
+                    theTransac[countTrs] = new Check(baseDate, "", "", 0m, "");
+                else if (gc[TYPE].ToString().ToUpper() == "DEBIT")
+                    theTransac[countTrs] = new Debit(baseDate, "", "", 0m);
+                else if (gc[TYPE].ToString().ToUpper() == "DEPOSIT")
+                    theTransac[countTrs] = new Deposit(baseDate, "", "", 0m);
                 else
                     Debug.WriteLine("Error: Undefined transaction type");
 
-                //transaction property parser
-                if (theTransac[countMatch].GetType() == typeof(Check))
-                    theTransac[countMatch].Checknum = gc[(int)Property.CHECKNUM].ToString();
+                //if check fill checknum prop
+                if (theTransac[countTrs].GetType() == typeof(Check))
+                    theTransac[countTrs].Checknum = gc[CHECKNUM].ToString();
                 else
                     Debug.WriteLine("Info: Transac is not a Check");
-                //Date
+                
+                //parse date
                 try
                 {
-                    string str = gc[(int)Property.DATE].ToString();
-                    date = Convert.ToDateTime(str, culture);
-                    theTransac[countMatch].Date = date;
+                    string str = gc[DATE].ToString();
+                    baseDate = Convert.ToDateTime(str, culture);
+                    theTransac[countTrs].Date = baseDate;
                 }
                 catch (FormatException e)
-                {
-                    Debug.WriteLine("Error: Check the Data input format");
-                }
+                { Debug.WriteLine("Error: Check the Data input format"); }
 
-                theTransac[countMatch].Description = gc[(int)Property.DESCRIPTION].ToString();
-                theTransac[countMatch].Category = gc[(int)Property.CATEGORY].ToString();
-                theTransac[countMatch].Amount = Decimal.Parse(gc[(int)Property.AMOUNT].ToString());
-
-                this.Add(theTransac[countMatch]);
-                countMatch++;
+                //parse everything else
+                theTransac[countTrs].Description = gc[DESCRIPTION].ToString();
+                theTransac[countTrs].Category = gc[CATEGORY].ToString();
+                theTransac[countTrs].Amount = Decimal.Parse(gc[AMOUNT].ToString());
+                
+                //add to transaction class
+                this.Add(theTransac[countTrs]);
+                countTrs++;
             }
-            /*Put in the xml data, 
-             Parse it for each object
-             Look to see if is a check, debit or deposit
-             Create the appropiate obj and fill it with the data
-             Add it to the current list with Add(t)*/
         }
         public decimal Balance
         {
