52d6633166c6802c6c29e9ceaf41e28b18c1ad8e
MikeB <mikebendorf11@gmail.com>
2018-05-30 10:55:31 -0700

    MVC 2 controllers, user and default

--- 3_webApps/Projects/Final/Controllers/DefaultController.cs
+++ 3_webApps/Projects/Final/Controllers/DefaultController.cs
@@ -20,61 +20,20 @@ namespace Final.Controllers
         {
             return View(db.vClasses.ToList());
         }
-        public ActionResult MyCourses()
-        {
-            if (Session["User"] != null)
-            {
-                return View();
-            }
-            return RedirectToAction("Login");
-        }
-        public ActionResult Register()
-        {
-            return View();
-        }
-         
-        //[HttpPost]
-        public ActionResult Login(string Login, string Password)
-        {
-            //Login = Password = "";
-            OleDbConnection objOleCon = new OleDbConnection();
-            objOleCon.ConnectionString = strOledbConnection;
-            OleDbCommand objCmd = new OleDbCommand("Select count(*) from [vStudents] where StudentLogin like '"+Login+ "' AND StudentPassword LIKE '" +Password+ "'", objOleCon);
-            //objCmd.Parameters.AddWithValue("@StudentLogin", "'"+Login+"'");
-            //objCmd.Parameters.AddWithValue("@StudentPassword", "'"+Password+"'");
-            try
-            {
-                objOleCon.Open();
-                
-                int i = (Int32)objCmd.ExecuteScalar();
-                 
-                
-                if ( i == 1)
-                {
-                    ViewBag.Message = "Valid User";
-                    Session["User"] = "valid";
-                }
-                else
-                {
-                    ViewBag.Message = "User not found";
-                }
-            }
-            catch (Exception ex) { ViewBag.Message = ex.ToString(); }
-            finally
-            {
-                objOleCon.Close();
-            }
-            return View();
-        }
+    
         public ActionResult NewLogin()
         {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult ValidateLogin()
-        {
-
-            return RedirectToAction("Login");
+            if (Session["User"] != null) //already logged in
+            {
+                ViewBag.Message = "You are already logged in";
+                return RedirectToAction("MyCourses", "User");
+            }
+            else
+            {
+                ViewBag.Message = "Create a new user";
+                return View();
+            }
+            
         }
     }
 }
---
+++ 3_webApps/Projects/Final/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Final.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace Final.Controllers
+{
+    public class UserController : Controller
+    {
+        DB_122058_test2Entities1 db = new DB_122058_test2Entities1();
+        string strOledbConnection = ConfigurationManager.ConnectionStrings["RemoteServer"].ConnectionString;
+
+        public ActionResult Login(string Login, string Password)
+        {
+            if (Session["User"] == null && Request.HttpMethod != "POST") //not logged in
+            { 
+                ViewBag.Message = "Please Login";
+                return View();
+            }
+            else if (Session["User"] == null && Request.HttpMethod == "POST")
+            {
+                OleDbConnection objOleCon = new OleDbConnection();
+                objOleCon.ConnectionString = strOledbConnection;
+                OleDbCommand objCmd = new OleDbCommand("Select count(*) from [vStudents] where StudentLogin like '" + Login + "' AND StudentPassword LIKE '" + Password + "'", objOleCon);
+                //objCmd.Parameters.AddWithValue("@StudentLogin", "'"+Login+"'");
+                //objCmd.Parameters.AddWithValue("@StudentPassword", "'"+Password+"'");
+                try
+                {
+                    objOleCon.Open();
+                    if ((Int32)objCmd.ExecuteScalar() == 1)
+                    {
+                        ViewBag.Message = "Welcome!";
+                        Session["User"] = "valid";                       
+                        return RedirectToAction("MyCourses");
+                    }
+                    else
+                        ViewBag.Message = "User not found"; 
+                }
+                catch (Exception ex){ViewBag.Message = ex.ToString();}
+                finally { objOleCon.Close(); }
+            }
+            else if (Session["User"] != null) 
+            {
+                ViewBag.Message = "You are already logged in";
+                return RedirectToAction("MyCourses");
+            }
+            return View();
+        }
+        public ActionResult MyCourses()
+        {
+            if (Session["User"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Register()
+        {
+            if (Session["User"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
