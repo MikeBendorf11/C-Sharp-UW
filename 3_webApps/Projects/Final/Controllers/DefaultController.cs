ecced2b93e5b83137772938e943e1102921ac5f0
MikeB <mikebendorf11@gmail.com>
2018-05-29 21:44:22 -0700

    MVC Db connection, string match using like and single quotes

--- 3_webApps/Projects/Final/Controllers/DefaultController.cs
+++ 3_webApps/Projects/Final/Controllers/DefaultController.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Final.Models;
-
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
 
 namespace Final.Controllers
 {
     public class DefaultController : Controller
     {
         DB_122058_test2Entities1 db = new DB_122058_test2Entities1();
+        string strOledbConnection = ConfigurationManager.ConnectionStrings["RemoteServer"].ConnectionString;
+
         //GET: Default
         public ActionResult Courses()
         {
@@ -18,19 +22,59 @@ namespace Final.Controllers
         }
         public ActionResult MyCourses()
         {
-            return View();
+            if (Session["User"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Login");
         }
         public ActionResult Register()
         {
             return View();
         }
-        public ActionResult Login()
+         
+        //[HttpPost]
+        public ActionResult Login(string Login, string Password)
         {
+            //Login = Password = "";
+            OleDbConnection objOleCon = new OleDbConnection();
+            objOleCon.ConnectionString = strOledbConnection;
+            OleDbCommand objCmd = new OleDbCommand("Select count(*) from [vStudents] where StudentLogin like '"+Login+ "' AND StudentPassword LIKE '" +Password+ "'", objOleCon);
+            //objCmd.Parameters.AddWithValue("@StudentLogin", "'"+Login+"'");
+            //objCmd.Parameters.AddWithValue("@StudentPassword", "'"+Password+"'");
+            try
+            {
+                objOleCon.Open();
+                
+                int i = (Int32)objCmd.ExecuteScalar();
+                 
+                
+                if ( i == 1)
+                {
+                    ViewBag.Message = "Valid User";
+                    Session["User"] = "valid";
+                }
+                else
+                {
+                    ViewBag.Message = "User not found";
+                }
+            }
+            catch (Exception ex) { ViewBag.Message = ex.ToString(); }
+            finally
+            {
+                objOleCon.Close();
+            }
             return View();
         }
         public ActionResult NewLogin()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult ValidateLogin()
+        {
+
+            return RedirectToAction("Login");
+        }
     }
-} 
+}
